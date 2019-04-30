a921fbaef33e589a036fda925b8bfb6d5afb6567
AgatheP <agathe.prioleau@gmal.com>
2019-04-30 16:49:07 +0200

    config twitter

---
+++ src/actions/config.js
@@ -0,0 +1,6 @@
+module.export= {
+  consumer_key:         'gy6M7sWQ4ULsh8DlTUoIHBDE',
+  consumer_secret:      'z8LNZrLTmplbhORvBnzln9DYKdT6KBc52Tb5ZY95WQqxqEaS11',
+  access_token:         '1123163924990517250-lovPJ15GuAv2j2OENGyW294d1Lnq4u',
+  access_token_secret:  't7TB4BHYwdzHLN1gsPsdnYfrDsp3uc19IxIzv8piKtirc',
+}
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
-console.log("twiter start!");
+console.log("twiter is starting!!!");
+var Twit= require('twit'); //Import twit
+var config = require('./config');
+var T = new Twit(config);

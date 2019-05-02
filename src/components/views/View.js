953d93eaf9d0547113ea75ed7ae0d59b319fb3da
LaurineSJK <lol-o45@live.fr>
2019-05-02 23:52:11 +0200

    chart

--- src/components/views/View.js
+++ src/components/views/View.js
@@ -4,5 +4,6 @@ import Header from '../Header'
 export default (state) =>
   h('div', {}, [
     Header(state.currentPage),
-    h('h1', {}, ['Proportion of twits including pet emojis, by country', 'Hashtags associated with each pet emoji', 'Comparative usage of choosen pet emojis'][state.currentPage])
+    h('h1', {}, ['Proportion of twits including pet emojis, by country', 'Hashtags associated with each pet emoji', 'Comparative usage of choosen pet emojis'][state.currentPage]),
+    // currently not displaying anything// Charts({ mydata: [1,2,3,4] })
   ])

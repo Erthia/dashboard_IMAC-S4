d9f885158576679db3846b31c25ca8947e7853bd
Laurine <lol-o45@live.fr>
2019-05-05 19:45:42 +0200

    display (ugly) Charts

--- src/components/ChartsTemplate.js
+++ src/components/ChartsTemplate.js
@@ -2,15 +2,15 @@ import { h } from 'hyperapp'
 import Chart from 'chart.js'
 
 
-export const pieTemplate = (data) => {
+export const PieTemplate = (props) => {
 	type: 'pie',
   data: {
-    labels: data.labels, // => "Cat", "Dog", "Rabbit", "Bird", "Snake", "Fish", "Turtle", "Mouse"
+    labels: ['Cat', 'Dog', 'Rabbit', 'Bird', 'Snake', 'Fish', 'Turtle', 'Mouse'],//props.labels, // => "Cat", "Dog", "Rabbit", "Bird", "Snake", "Fish", "Turtle", "Mouse"
     datasets: [{
       labels: "Pourcentage of emojis by species",
       backgroundColor: ['#3e95cd', '#f4ac41', '#8e5ea2', '#3cba9f', '#f46441', '#e8c3b9', '#c45850', '#1d15da'],
       borderColor: 'rgb(66, 134, 244)',
-      data: data.values // our number of occurrence
+      data: props.values, // our number of occurrence
     }]
   },
   options: {
@@ -18,23 +18,25 @@ export const pieTemplate = (data) => {
       display: true,
       text: "Pourcentage of emojis by species"
     },
+  }
 }
 
-export const horizontalBarTemplate = (data) => {
+export const HorizontalBarTemplate = (props) => {
 	type: 'horizontalBar',
   data: {
-    labels: data.labels, // => "France", "Italy", "Germany", etc...
+    labels: props.labels, // => "France", "Italy", "Germany", etc...
     datasets: [{
       labels: "Number of emojis included in tweets",
       backgroundColor: ['#3e95cd', '#f4ac41', '#8e5ea2', '#3cba9f', '#f46441', '#e8c3b9', '#c45850', '#1d15da'],
       borderColor: 'rgb(66, 134, 244)',
-      data: data.values // our number of occurrence
+      data: props.values, // our number of occurrence
     }]
   },
   options: {
   	legend: { display: false },
     title: {
       display: true,
-      text: "Pourcentage of emojis by country"
+      text: "Pourcentage of emojis by country",
     },
+  }
 }
--- src/components/views/View.js
+++ src/components/views/View.js
@@ -1,9 +1,11 @@
 import { h } from 'hyperapp'
 import Header from '../Header'
+//import {PieTemplate, HorizontalBarTemplate} from '../ChartsTemplate'
+import Charts from '../Charts'
 
 export default (state) =>
   h('div', {}, [
     Header(state.currentPage),
     h('h1', {}, ['Proportion of twits including pet emojis, by country', 'Hashtags associated with each pet emoji', 'Comparative usage of choosen pet emojis'][state.currentPage]),
-    // currently not displaying anything// Charts({ mydata: [1,2,3,4] })
+  	Charts(),
   ])

2590dbee6b38d8f93a89d9b46c0d7ff7a5199a17
Erthia <catemi3@orange.fr>
2019-05-06 11:02:11 +0200

    Change state, and gitignore

--- src/state/index.js
+++ src/state/index.js
@@ -1,3 +1,39 @@
 export default{
-  currentPage : 2
+  currentPage: 2,
+  pets:[
+    {
+      name: 'dog',
+      selected: true,
+      emoticon:[🐶,🐕,🐩]
+    },
+    {
+      name: 'cat',
+      selected: true,
+      emoticon: [🐱,🐈,😺]
+    },
+    {
+      name: 'bunny',
+      selected: false,
+      emoticon: [🐰,🐇]
+    },
+    {
+        name: 'reptile',
+        selected: false,
+        emoticon: [🐍,🐢,🦎]
+    },{
+      name: 'mouse',
+      selected: false,
+      emoticon: [🐭,🐀,🐁]
+    },
+    {
+      name: 'fish',
+      selected: false,
+      emoticon: [🐠,🐟,🐡]
+    },
+    {
+        name: 'bird',
+        selected: false,
+        emoticon:[🐔,🐧,🐦,🐤,🐣,🐥,🕊,🦆,🦅,🦉]
+    }
+  ]
 }
